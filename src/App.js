28ea50998c4600f57352ad7f06c2e875400ba600
Rana Soyak <rselvasoyak@gmail.com>
2023-02-12 10:40:33 -0500

    adding components to pseudocode

--- src/App.js
+++ src/App.js
@@ -23,14 +23,26 @@ export default App;
 
 // pseudo code
 
-// on load up
+// Main Component - App.js
+  // Home Page 
+  // Concert Search Page 
+  // Budget List Page 
+
+
+
+
+
+
+// on component mount
 
   // all pages have icon button to go to list
+  // Changing in between pages - display loading page for 2 seconds. 
 
-  // client will see index starter page
+  // client will see index starter page 
       // has 3 components, feature, form, collection of recommendations 
 
       // images interaction are stretch goals, specifically for component 1 and 3 for load up
+        // Have placeholder images for the concert image containers. 
 
       // form component 
         // contains name and budget input sections, onSubmit -> goes to 2nd module/view
@@ -41,6 +53,8 @@ export default App;
 
         // form component has two inputs - artist and location 
           // onSubmit generates list - that filters out with inputs 
+          // If the API call does not have a return based on the input, display error page? 
+          // When API call is happening - display loading page for 3 seconds. 
 
         // list component - shows filtered concert options with should display relevant information (e.g. Performer, dates, venue, ticket price range).
           // each list has a plus button to add to saved list
@@ -48,7 +62,7 @@ export default App;
         // has back button
 
 
-    // 3rd module/view
+    // 3rd module/view 
         // contains summary of list, can edit list and budget
         // visual indication of under budget, at budget, over budget
         // back button
