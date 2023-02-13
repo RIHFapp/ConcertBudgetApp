a9cf517df5a844d70649a016ae497d60cbfe6c60
Hoi Wong <jameshf420@gmail.com>
2023-02-13 16:53:01 -0500

    style change on homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -19,17 +19,30 @@ const Homepage = ({name}) => {
   
     return(
       <div>
-        <h1>Budget vs Concert</h1>
+        <div className="featured">
+          <h1>Budget vs Concert</h1>
+          <div className="feature-image">
+          <img></img>
+          </div>
+        </div>
+      <div className="input-section">
         <h2>Welcome! lets Start planning your conecert list</h2>
-      <form action="submit">
-        <label htmlFor="newName"></label>
-        <input 
-        onChange={handleInput} type="text" id="newName" value={userListName} placeholder="name of your list" />
-        <label htmlFor=""></label>
-        <input 
-         type="text" id="newName" placeholder="your budget" />
-        <button onClick={handleSubmit}>Add List</button>
-      </form>
+        <form action="submit">
+          <label htmlFor="newName"></label>
+          <input 
+          onChange={handleInput} type="text" id="newName" value={userListName} placeholder="name of your list" />
+          <label htmlFor=""></label>
+          <input 
+          type="text" id="newName" placeholder="your budget" />
+          <button onClick={handleSubmit}>Add List</button>
+        </form>
+      </div>
+      <div className="suggested">
+        <div className="small-img"></div>
+        <div className="small-img"></div>
+        <div className="small-img"></div>
+        <div className="small-img"></div>
+      </div>
       {/* <ul>
         {name.map ( (name) => {
           return (
