2a8f4b38655730e9e7da2c2d4b2515c4a979795b
Rana Soyak <rselvasoyak@gmail.com>
2023-02-14 15:57:06 -0500

    api call filterin initiated - will move to contentSearchPage branch

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -41,11 +41,12 @@ useEffect (() => {
         classificationName:"music"
       }
     }).then((res) => {
-      const list = [...res.data._embedded.events]
+      const list = res.data._embedded.events.filter((event) => {
+        return event;
+      })
       console.log(list);
       setApiRes(list); 
     })
-
   }  
 
 },[artist, city])
@@ -89,10 +90,10 @@ useEffect (() => {
                       // const minPrice = concertInfo.priceRanges[0].min;
                       const maxPrice = concertInfo.priceRanges !== undefined
                         ? 
-                         concertInfo.priceRanges[0].max
-                         : 
+                        concertInfo.priceRanges[0].max
+                        : 
                         'To be announced';
-                     
+                    
 
                       const concertImg = concertInfo.images[3].url;
                       const key = concertInfo.id;
@@ -100,7 +101,11 @@ useEffect (() => {
                         <li 
                         key = {key}
                         className="concertResponse wrapper">
-                          <button> + </button>
+                          { 
+                            concertInfo.priceRanges !== undefined ? ( 
+                              <button> + </button>
+                             ) : null
+                          }
                           <div className="concertListInfo">
                             <p> {name} </p>
                             <p> {eventDate} </p>
