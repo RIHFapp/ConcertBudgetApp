31329aff1205cc13fbc22df1ab717dead9def7be
Hoi Wong <jameshf420@gmail.com>
2023-02-16 15:53:31 -0500

    added div

--- src/Components/PublishList.js
+++ src/Components/PublishList.js
@@ -1,6 +1,24 @@
 const PublishList = () => {
     return(
         <>
+            <section>
+                <div className="wrapper">
+                    <div className="myList">
+                        <h2>Created List</h2>
+                        <ul>
+                            <h3>Rana's List</h3>
+                            <li>
+                            <p>BEYONCÉ - RENAISSANCE WORLD TOUR</p>
+                            <p>2023-09-11</p>
+                            <p>Vancouver</p>
+                            <p>BC Place</p>
+                            <p>581</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </section>
         </>
     )
 }
