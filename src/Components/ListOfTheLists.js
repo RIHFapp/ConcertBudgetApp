17ba97633cde028852c746840def4d7a9c9e3160
izazw <iza@wasielak.eu>
2023-02-16 16:36:10 -0500

    ListOfTheLists.js html structure

---
+++ src/Components/ListOfTheLists.js
@@ -0,0 +1,36 @@
+const ListOfTheLists = () => {
+
+      return (
+         <>
+            <div className="wrapper">
+               <h2> List of created list</h2> 
+                  <div>
+                     <h3>First Web Developers' paycheck concert list</h3>
+                     <p>budget: 1000 CAD </p>
+                        <ul>
+                           <li> Beyonce [date] 200 CAD </li>
+                           <li> concert2 100 CAD </li>
+                           <li> concert3 20 CAD</li>
+                           <li> concert4 10 CAD</li>
+                           <li> concert5 670 CAD </li>
+                        </ul>
+                  </div>
+                  <div>
+                     <h3>Second Web Developers' paycheck concert list</h3>
+                     <p>budget: 1000 CAD </p>
+                        <ul>
+                           <li> concert1 200 CAD </li>
+                           <li> concert2 100 CAD </li>
+                           <li> concert3 20 CAD</li>
+                           <li> concert4 10 CAD</li>
+                           <li> concert5 670 CAD </li>
+                        </ul>
+                  </div>
+            </div>
+       
+         </>
+   )
+
+}
+
+export default ListOfTheLists;
