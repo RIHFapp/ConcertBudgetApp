503ad8b94b114d39c803e8524ad73d0cd6be047c
izazw <iza@wasielak.eu>
2023-02-17 10:28:56 -0500

    styling fixes

--- src/Components/ListOfTheLists.js
+++ src/Components/ListOfTheLists.js
@@ -2,31 +2,18 @@ const ListOfTheLists = () => {
 
       return (
          <>
-            <div className="wrapper">
+            <div className="wrapper listOfTheListsContainer">
                <h2> List of created list</h2> 
-                  <div>
+                  <div className="listContainer">
                      <h3>First Web Developers' paycheck concert list</h3>
                      <p>budget: 1000 CAD </p>
-                        <ul>
-                           <li> Beyonce [date] 200 CAD </li>
-                           <li> concert2 100 CAD </li>
-                           <li> concert3 20 CAD</li>
-                           <li> concert4 10 CAD</li>
-                           <li> concert5 670 CAD </li>
-                        </ul>
                   </div>
-                  <div>
+                  <div className="listContainer">
                      <h3>Second Web Developers' paycheck concert list</h3>
                      <p>budget: 1000 CAD </p>
-                        <ul>
-                           <li> concert1 200 CAD </li>
-                           <li> concert2 100 CAD </li>
-                           <li> concert3 20 CAD</li>
-                           <li> concert4 10 CAD</li>
-                           <li> concert5 670 CAD </li>
-                        </ul>
                   </div>
             </div>
+            <button id="LOLButton">back</button>
        
          </>
    )
