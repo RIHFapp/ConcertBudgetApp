f155a79717cd855fb86e2a76d93e3f16f08ce40e
izazw <iza@wasielak.eu>
2023-02-18 16:59:50 -0500

    useEffect structure, pseudocode

--- src/Components/ListWithKeys.js
+++ src/Components/ListWithKeys.js
@@ -1,19 +1,66 @@
 import { Link } from "react-router-dom";
+import firebase from "../firebase";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const ListWithKeys = (props) => {
 
-    console.log(props.passEditId);
-    console.log(props.passShareId);
+    // console.log(props.passEditId);
+    // console.log(props.passShareId);
+
+
+    //connecting to the firebase data
+    //pulling the information about the list 
+        //h2 => changing the h2 to the Budget List
+        //span.budgetValue => renders the budget list value 
+     //displaying the budgetConcertContent list:
+        //name, date, city, location, price, 
+        //additionally number of tickets
+    //event handler on the Remove Tickets button 
+    //useState = to use in the span.totalTicketPrice? to check
+   
+    //adding the CAD to the price 
+
+
+    //to discuss:
+     //adding the number of tickets to the firebase data structure? -> in the budgetConcertContent
+     //displaying the number of tickets
+      //event handler on a "+" and "-" buttons => the buttons are changing the total price
+        //why console.logs above?
+
+
+
+const [myEditableList, setMyEditableList] = useState([]);
+
+
+        // useEffect( () => {
+     
+      const database = getDatabase(firebase);
+        const dbRef = ref(database);
+        onValue(dbRef, (response) => {
+           const listsData = response.val();
+           const newState = [];
+           for (let key in listsData) {
+              newState.push(listsData[key]);
+           }
+        //    setLists(newState);
+        })
+     
+        // }, [])
+
     return(
         <>
             <section>
                 <div className="wrapper">
                     <div className="detaliedList">
+                        <p>{list}</p>
+                    
                         <h2>Rana but poor after joining bootcamp</h2>
                         
                         <div className="listHeading">
-                        <h3>Concernt 6000 </h3>
+                        <h3>Concernt <span id="budgetValue">6000</span> </h3>
                         <h3>vs</h3>
-                        <h3>Budget 10</h3>
+                        <h3>Budget <span id="totalTicketPrice">10</span></h3>
                         </div>
                         
                         <ul>
