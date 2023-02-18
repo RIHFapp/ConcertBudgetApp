b43e07bc67c4f98e169b435466d7bb6a34c4bb35
Hoi Wong <jameshf420@gmail.com>
2023-02-17 22:29:04 -0500

    fixing deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 
-import { useState, useEffect } from "react";
+import { useState} from "react";
 
 // Componetns
 import HomePage from "./Components/HomePage";
