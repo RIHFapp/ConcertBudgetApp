b4a66c03069ab2c1c96cd4f096d71621961f30d2
izazw <iza@wasielak.eu>
2023-02-19 15:49:54 -0500

    Cleaning up the code, indentation etc.

--- src/Components/ListWithKeys.js
+++ src/Components/ListWithKeys.js
@@ -1,3 +1,10 @@
+//to be solved:
+// remove ticket functionality
+//changing get() to onValue 
+//if logic for number of tickets is ready, add number of tickets functionality
+//pairing the paramId and the object from Firebase (right now the objectkey is hardcoded, check const keyToMyList)
+//responsive design
+
 import { Link } from "react-router-dom";
 import firebase from "../firebase";
 import { getDatabase, ref, onValue, get } from "firebase/database";
@@ -5,7 +12,10 @@ import { useEffect, useState } from "react";
 
 const ListWithKeys = () => {
 
+//hardcoded key to the firebase, to be resolved with the paramId
 const keyToMyList = "-NOeqhYOho6hRXNgky2j"
+
+//states 
 const [nameOfTheList, setNameOfTheList] = useState("Your list");
 const [budgetValue, setBudgetValue] = useState("0");
 const [listOfConcerts, setListOfConcerts] = useState([]);
@@ -19,7 +29,7 @@ const checkoutTheData = (name, budget, concerts)=> {
     setListOfConcerts(concerts);
 }
 
-
+//function summing up the prices of tickets
 const sumOfPrices = (arrayOfConcerts) => {
 let totalPrice = 0
     for (let price of arrayOfConcerts) {
@@ -29,12 +39,24 @@ let totalPrice = 0
         return totalPrice
 }
 
+
 useEffect( () => {
 
     const database = getDatabase(firebase);
     const dbRef = ref(database);
 
-    //Please keep in mind that I'm not sure if it's gonna be get, or rather onValue!
+    //Please keep in mind that it's going to be rather onValue!
+    // onValue(dbRef, (response) => {
+//         const listContent = response.val();
+//         console.log(listContent)
+//         const newListInfo = [];
+        
+//         for (let key in listContent) {
+//             newListInfo.push(listContent[key]);
+//             console.log(newListInfo)
+//            }
+//            setMyEditableList(newListInfo);
+//         })
 
     get(dbRef).then((snapshot) => {
         // One of the returned values is a method called ".exists()", which will return a boolean value for whether there is a returned value from our "get" function 
@@ -49,7 +71,6 @@ useEffect( () => {
         
         setTotalTicketPrice(sumOfPrices(allChosenConcerts))
 
-
         } else {
         console.log("No data available")
         }
@@ -58,23 +79,17 @@ useEffect( () => {
     }) 
 }, [])  
 
-
-
     return(
         <>
             <section>
                 <div className="wrapper">
                     <div className="detaliedList">
-                        
-                    
                         <h2>{nameOfTheList}</h2>
-                        
                         <div className="listHeading">
-                        <h3>Concert <span id="budgetValue">{totalTicketPrice}</span> </h3>
-                        <h3>vs</h3>
-                        <h3>Budget <span id="totalTicketPrice">{budgetValue}</span></h3>
+                            <h3>Concert <span id="budgetValue">{totalTicketPrice}</span> </h3>
+                            <h3>vs</h3>
+                            <h3>Budget <span id="totalTicketPrice">{budgetValue}</span></h3>
                         </div>
-                        
                         <ul> 
                             <li className="listTags inKeys">
                                 <div className="listConcertTags">
@@ -91,7 +106,6 @@ useEffect( () => {
                             {
                                 listOfConcerts.map( (oneConcert) => {
                                     return (
-                                   
                                         <li className="one" key={oneConcert.key}>
                                             <p>{oneConcert.name}</p>
                                             <p>{oneConcert.eventDate}</p>
@@ -110,45 +124,10 @@ useEffect( () => {
 
                 </div>
                 <Link to={`/`}>
-            <button id="LOLButton">back</button>
-            </Link>
+                    <button id="LOLButton">back</button>
+                </Link>
             </section>
         </>
     )
 }
 export default ListWithKeys;
-
-// onValue(dbRef, (response) => {
-//         const listContent = response.val();
-//         console.log(listContent)
-//         const newListInfo = [];
-        
-//         for (let key in listContent) {
-//             newListInfo.push(listContent[key]);
-//             console.log(newListInfo)
-//            }
-//            setMyEditableList(newListInfo);
-//         })
-
-   // console.log(props.passEditId);
-    // console.log(props.passShareId);
-    //
-//editKey must be send as a props from the List of the lists
-    //connecting to the firebase data
-    //pulling the information about the list 
-        //h2 => changing the h2 to the Budget List
-        //span.budgetValue => renders the budget list value 
-     //displaying the budgetConcertContent list:
-        //name, date, city, location, price, 
-        //additionally number of tickets
-    //event handler on the Remove Tickets button 
-    //useState = to use in the span.totalTicketPrice? to check
-   
-    //adding the CAD to the price 
-
-//to check: can I download only one item from val (does .val accepts any query)
-    //to discuss:
-     //adding the number of tickets to the firebase data structure? -> in the budgetConcertContent
-     //displaying the number of tickets
-      //event handler on a "+" and "-" buttons => the buttons are changing the total price
-        //why console.logs above?
