7fd3bc661805ddf8d1757b7090b1ba93cc2960d5
izazw <iza@wasielak.eu>
2023-02-18 19:52:36 -0500

    displaying h2 from the object (list name)

--- src/Components/ListWithKeys.js
+++ src/Components/ListWithKeys.js
@@ -1,19 +1,14 @@
 import { Link } from "react-router-dom";
 import firebase from "../firebase";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, get } from "firebase/database";
 import { useEffect, useState } from "react";
 
 const ListWithKeys = (props) => {
 
     // console.log(props.passEditId);
     // console.log(props.passShareId);
-
     //
-
 //editKey must be send as a props from the List of the lists
-
-
-
     //connecting to the firebase data
     //pulling the information about the list 
         //h2 => changing the h2 to the Budget List
@@ -33,26 +28,54 @@ const ListWithKeys = (props) => {
       //event handler on a "+" and "-" buttons => the buttons are changing the total price
         //why console.logs above?
 
+const keyToMyList = "-NOaq6mPoOGyMdREbJ5E"
 
 
 const [myEditableList, setMyEditableList] = useState([]);
+const [nameOfTheList, setNameOfTheList] = useState()
+
+const checkoutTheData = (name)=> {
+    setNameOfTheList(name)
+}
 
 
 useEffect( () => {
      
 const database = getDatabase(firebase);
 const dbRef = ref(database);
+
+get(dbRef).then((snapshot) => {
+    // One of the returned values is a method called ".exists()", which will return a boolean value for whether there is a returned value from our "get" function 
+    if(snapshot.exists()){
+      // We call `.val()` on our snapshot to get the contents of our data. The returned data will be an object that we can  iterate through later
+    console.log(snapshot.val())
+    const allTheLists = snapshot.val();
+    // for (let property in allTheLists) {
+    //     console.log(allTheLists[property])
+    // }
+
+
+if (allTheLists.hasOwnProperty("-NOaq6mPoOGyMdREbJ5E") === true) {
+    console.log(allTheLists["-NOaq6mPoOGyMdREbJ5E"].listname)
+    
+    const nameFromList = allTheLists["-NOaq6mPoOGyMdREbJ5E"].listname;
+    checkoutTheData(nameFromList)
+    console.log(allTheLists["-NOaq6mPoOGyMdREbJ5E"].userBudget)
+    console.log(allTheLists["-NOaq6mPoOGyMdREbJ5E"].budgetConcertContent)
+}
+
+    //   for (let key in allTheLists) {
+    //     myListToDisplay.push(allTheLists[key]);
+    //     console.log(myListToDisplay)
+    //   }
+    }else{
+      console.log("No data available")
+    }
+  }).catch((error) => {
+    console.log(error)
+  })
         
-onValue(dbRef, (response) => {
-        const listContent = response.val();
-        console.log(listContent)
-        const newListInfo = [];
-        
-        for (let key in listContent) {
-            newListInfo.push(listContent[key]);
-           }
-           setMyEditableList(newListInfo);
-        })
+
      
         }, [])
 
@@ -61,9 +84,9 @@ onValue(dbRef, (response) => {
             <section>
                 <div className="wrapper">
                     <div className="detaliedList">
-                        <p>{"list"}</p>
+                        <p></p>
                     
-                        <h2>Rana but poor after joining bootcamp</h2>
+                        <h2>{nameOfTheList} haha</h2>
                         
                         <div className="listHeading">
                         <h3>Concernt <span id="budgetValue">6000</span> </h3>
@@ -143,3 +166,15 @@ onValue(dbRef, (response) => {
     )
 }
 export default ListWithKeys;
+
+// onValue(dbRef, (response) => {
+//         const listContent = response.val();
+//         console.log(listContent)
+//         const newListInfo = [];
+        
+//         for (let key in listContent) {
+//             newListInfo.push(listContent[key]);
+//             console.log(newListInfo)
+//            }
+//            setMyEditableList(newListInfo);
+//         })
