9a00b85ebc5e22412c6a356847dea85a5891ecb6
Hoi Wong <jameshf420@gmail.com>
2023-02-20 00:21:00 -0500

    footer debugg

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,17 @@
 const Footer = () => {
     return (
-        <p> Created by; 
-            <a href="https://izacodes.me"> Iza Zamorska-Wasielak </a> |
-            <a href="https://farhandev.ca/"> Farhan Siddiqi </a> | 
-            <a href="https://hoi-jw.com"> Hoi Wong </a> |
-            <a href="https://ranasoyakcodes.dev"> Rana Soyak</a> |
-        </p>
+        <>
+        <section className="footerStyle">
+            <div className="wrapper">
+                <p> Created by; 
+                    <a href="https://izacodes.me"> Iza Zamorska-Wasielak </a> |
+                    <a href="https://farhandev.ca/"> Farhan Siddiqi </a> | 
+                    <a href="https://hoi-jw.com"> Hoi Wong </a> |
+                    <a href="https://ranasoyakcodes.dev"> Rana Soyak</a> |
+                </p>
+            </div>
+        </section>
+        </>
     )
 }
 
